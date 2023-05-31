b3abdd89078eeb0dc5a7b891308077567212a34b
William-ops-crypto <william.s.91@hotmail.com>
2023-05-31 11:46:18 -0300

    Debug

--- src/main/java/com/devsuperior/dslist/controllers/GameBuscarController.java
+++ src/main/java/com/devsuperior/dslist/controllers/GameBuscarController.java
@@ -3,6 +3,7 @@ package com.devsuperior.dslist.controllers;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -16,6 +17,7 @@ import com.devsuperior.dslist.services.GameBuscarService;
 
 
 @RestController
+@CrossOrigin(origins = "https://catalogo-games-production.up.railway.app") 
 @RequestMapping(value = "/buscar")
 
 
@@ -27,7 +29,7 @@ public class GameBuscarController {
 	
 	
 	
-	@GetMapping("{title}")
+	@GetMapping(value = "/title")
 	
 	public List<Game> findByTitle(@RequestParam(value = "title", required = true)String title) {
 		List<Game> list = gameBuscarService.findByTitleLike(title);
